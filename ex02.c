6949be9d48b33def8df0a3ead8dbece12477afd9
youneswolf <younesbellakrid@gmail.com>
2023-05-04 12:06:55 +0100

    first_word

--- ex02.c
+++ ex02.c
@@ -6,13 +6,13 @@ if(argc==2){
     while(argv[1][i]==' '  || argv[1][i]=='\t'){
         i++;
     }
-    while(argv[1][i]!='\0'  || argv[1][i]!=' '  || argv[1][i]!='\t'){
-        write(1,&str[i],1);
+    while(argv[1][i]!='\0'  && argv[1][i]!=' '  & argv[1][i]!='\t'){
+        write(1,&argv[1][i],1);
         i++;
     }
     
 }else{
-    write(1,'\n',1);
+    write(1,"\n",1);
 }
     return 0;
 }
