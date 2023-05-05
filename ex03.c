91cddac7b567d378354645796eab155a81944912
youneswolf <younesbellakrid@gmail.com>
2023-05-05 10:11:50 +0100

    rot_13

---
+++ ex03.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void rot_13(char *str){
+  int i=0;
+  while(str[i]){
+      if(str[i]>="a" && str[i]<="m" || str[i]>="A" && str[i]<="M"){
+          str[i]+=13;
+      }
+      if(str[i]>="n" && str[i]<="z" || str[i]>="N" && str[i]<="Z"){
+          str[i]-=13;
+      }
+      write(1,str[i],1);
+      i++;
+  }
+  
+}
