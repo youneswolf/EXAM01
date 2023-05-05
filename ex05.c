e3d2201e1a07b44cb6c70fe26e82ef44fa11a72a
youneswolf <younesbellakrid@gmail.com>
2023-05-05 10:35:30 +0100

    inter

---
+++ ex05.c
@@ -0,0 +1,16 @@
+void rot_13(char *str1,char *str2){
+  int i=0;
+  int j=0;
+while(str1[i]){
+      while(str2[j]){
+            if(str1[i]==str2[j]){
+               write(1,str1[i],1);
+                i++;
+                j=0;
+                            }
+            j++;
+                    }
+            i++;
+               }
+  
+}
